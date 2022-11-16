cd35bcff3fff96faa62361f410a0980bc5bbbb21
Adrián Medve <medveadrian03@gmail.com>
2022-11-16 14:27:34 +0100

    2. feladat kész

--- ProgrammingCourse/Program.cs
+++ ProgrammingCourse/Program.cs
@@ -3,14 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ProgrammingCourse
 {
     class Program
     {
+        static List<Kurzus> kurzus = new List<Kurzus>();
         static void Main(string[] args)
         {
+            MasodikFeladat();
+            HarmadikFeladat();
+        }
 
+        private static void HarmadikFeladat()
+        {
+            Console.WriteLine("3. ");
+        }
+
+        private static void MasodikFeladat()
+        {
+            using (StreamReader sr = new StreamReader("coursedata.csv"))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] tmp = sr.ReadLine().Split(';');
+                    kurzus.Add(new Kurzus(tmp[0], Convert.ToChar(tmp[1]), Convert.ToInt32(tmp[2]), Convert.ToInt32(tmp[3]), Convert.ToInt32(tmp[4]), Convert.ToInt32(tmp[5]), Convert.ToInt32(tmp[6]), Convert.ToInt32(tmp[7])));
+                }
+            }
         }
     }
 }
